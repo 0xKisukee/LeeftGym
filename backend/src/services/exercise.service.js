e9abfba51efd35cb97aeba19e892f6f1e8712172
0xKisukee <aitbenali0502@gmail.com>
2025-04-05 21:16:36 +0200

    fix services files names

--- backend/src/services/exercise.service.js
+++ backend/src/services/exercise.service.js
@@ -1,17 +1,60 @@
 const {AppError} = require('../utils/appError');
+const {Op} = require('sequelize');
 const models = require('../../database/models');
 
-async function show(id) {
-    return await models.Exercise.findByPk(id);
-}
+async function store(userId, workoutId, data) {
+    const workout = await models.Workout.findByPk(workoutId);
+    if (!workout) {
+        throw new AppError('Workout not found', 404);
+    }
+    if (userId !== workout.user_id) {
+        throw new AppError('You can\'t add an exercise this workout', 400);
+    }
 
-async function store(data) {
-    return await models.Exercise.create({
-        name: data.name,
+    // Check if exercise exists
+    const exercise = await models.Exercise.findByPk(data.exercise_id);
+    if (!exercise) {
+        throw new AppError('Exercise not found', 404);
+    }
+
+    // Calculate orderValue
+    let orderValue;
+    // Get amount of exercises in the workout
+    const workoutExercises = await models.WorkoutExercise.findAll({
+        where: {workout_id: workoutId}
+    });
+
+    if (!data.order) {
+        // Place this exercise at the last place
+        orderValue = workoutExercises.length + 1;
+    } else if (data.order > workoutExercises.length + 1) {
+        orderValue = workoutExercises.length + 1;
+    } else {
+        // Use the specified order
+        orderValue = data.order;
+
+        // If an exercise with this order already exists, shift other exercises
+        const existingExercises = await models.WorkoutExercise.findAll({
+            where: {
+                workout_id: workoutId,
+                order: {[Op.gte]: orderValue}
+            }
+        });
+
+        // Increment the order of all exercises that come after this one
+        for (const exercise of existingExercises) {
+            await exercise.update({order: exercise.order + 1});
+        }
+    }
+
+    return await models.WorkoutExercise.create({
+        workout_id: workoutId,
+        exercise_id: data.exercise_id,
+        order: orderValue,
+        rest_time: data.rest_time, // maybe add || 180
     });
 }
 
 module.exports = {
-    show,
     store,
 };
