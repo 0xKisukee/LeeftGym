5959c21f15ae428ff58d384ab642ba0d57c605e4
0xKisukee <aitbenali0502@gmail.com>
2025-04-09 21:37:41 +0200

    fetch user in home

--- mobile/app/(tabs)/home.jsx
+++ mobile/app/(tabs)/home.jsx
@@ -2,13 +2,28 @@ import { View } from "react-native";
 import { ScreenContainer } from "../../components/ScreenContainer";
 import { Title } from "../../components/StyledText";
 import { WorkoutFeed } from "../../components/WorkoutFeed";
+import { me } from "../../api/login";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+    const [userInfo, setUserInfo] = useState(null);
+
+    const fetchUser = async () => {
+        const infos = await me();
+        setUserInfo(infos);
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
         <ScreenContainer>
             <View className="flex-1">
                 <Title className="mb-4 mt-8">Fil d'actualité</Title>
-                <WorkoutFeed />
+                <WorkoutFeed
+                    userInfo={userInfo}
+                />
             </View>
         </ScreenContainer>
     );
