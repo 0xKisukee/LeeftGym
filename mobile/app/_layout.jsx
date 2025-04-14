f1060d31fc7d17988ee7847e10334371326f2484
0xKisukee <aitbenali0502@gmail.com>
2025-04-14 13:30:41 +0200

    use tanstack query

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -3,20 +3,24 @@ import {Stack} from "expo-router";
 import {ExoProvider} from "../contexts/ExoContext";
 import {UserProvider} from "../contexts/UserContext";
 import {BottomSheetProvider} from "../contexts/BottomSheetContext";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 export default function RootLayout() {
+    const queryClient = new QueryClient();
 
     return (
-        <ExoProvider>
-            <UserProvider>
-                <BottomSheetProvider>
-                    <Stack>
-                        <Stack.Screen name="index" options={{headerShown: false, gestureEnabled: false }}/>
-                        <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-                        <Stack.Screen name="(tabs)" options={{headerShown: false, gestureEnabled: false }}/>
-                    </Stack>
-                </BottomSheetProvider>
-            </UserProvider>
-        </ExoProvider>
+        <QueryClientProvider client={queryClient}>
+            <ExoProvider>
+                <UserProvider>
+                    <BottomSheetProvider>
+                        <Stack>
+                            <Stack.Screen name="index" options={{headerShown: false, gestureEnabled: false }}/>
+                            <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+                            <Stack.Screen name="(tabs)" options={{headerShown: false, gestureEnabled: false }}/>
+                        </Stack>
+                    </BottomSheetProvider>
+                </UserProvider>
+            </ExoProvider>
+        </QueryClientProvider>
     )
 }
